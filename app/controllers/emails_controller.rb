086054d7b6eead8ffdf40b9a34dc06fa61949d6a
Vivek D <vivster7@gmail.com>
2013-07-17 14:57:08 -0700

    use full URL for image; turned of async controller

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -8,7 +8,10 @@ class EmailsController < ApplicationController
 		@email = Email.new(text: params[:email][:text], image_url: ugly_id)
 
 		if @email.save!
-			EmailsWorker.perform_async(@email.id, ugly_id)	
+			#EmailsWorker.perform_async(@email.id, ugly_id)	
+			html = @email.text
+			kit = IMGKit.new(html)
+			file = kit.to_file('app/assets/images/'+ugly_id+'.jpg')
 			redirect_to email_path(@email)
 		else
 			render :new
