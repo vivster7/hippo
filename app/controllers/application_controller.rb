a67a7cd6f8fa6548c67ab7af5626f8b5c68b94ac
Vivek D <vivster7@gmail.com>
2013-07-23 17:08:42 -0700

    added authentication for requests made by the plugin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,12 +16,34 @@ class ApplicationController < ActionController::Base
   end
 
   def require_login
-    redirect_to auth_path unless current_user
+    puts registered?(params)
+    unless registered?(params)
+      #params will be sent before login if they are sent directly from the extension.
+      if params.has_key? :email
+        if params[:email].has_key? :text
+          if params[:email].has_key? :account
+            render inline: "<%='http://localhost:3000/auth/gplus'%>"
+          end
+        end
+      #no params sent; accessing directly via web service
+      else
+        redirect_to auth_path unless current_user
+      end
+    end
   end
 
   def current_user
     @current_user ||= User.where(id: session[:user_id]).first
   end
   helper_method :current_user
+
+  def registered?(params)
+    if params.has_key? :email
+      if params[:email].has_key? :account
+        return User.where(email: params[:email][:account]).count == 1
+      end
+    end
+    return false
+  end
 
 end
