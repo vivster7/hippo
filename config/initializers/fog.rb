55af863da3e537e601823a007802cd6f3e5a76b9
Vivek D <vivster7@gmail.com>
2013-07-31 11:16:59 -0700

    Use amazon storage locally

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -3,7 +3,9 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => Secret.AWS_access_key,        # required
     :aws_secret_access_key  => Secret.AWS_secret_key,        # required
-    :region                 => 'us-west-2'                   # optional, defaults to 'us-east-1'
+    :region                 => 'us-west-1',                  # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = 'hipaa-hippo/images'               # required
+  config.fog_directory  = 'hipaa-hippo'                     # required
+  config.fog_public     = false                                   # optional, defaults to true
+  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
