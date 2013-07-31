2d1d6e1f10ad3dc2dfdc7b90f29f0592c2ce40ca
Vivek D <vivster7@gmail.com>
2013-07-31 11:53:24 -0700

    changed initializers for heroku

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
   if Secret.respond_to?(:google_key)
     provider :gplus, Secret.google_key, Secret.google_secret, scope: 'userinfo.email, userinfo.profile'
+  else
+    provider :gplus, ENV['GPLUS_KEY'], ENV['GPLUS_SECRET'], scope: 'userinfo.email, userinfo.profile'
   end
 end
 OmniAuth.config.logger = Rails.logger
