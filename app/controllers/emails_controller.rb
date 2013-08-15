5499158929297009337a540312286a8e32008ccb
Vivek D <vivster7@gmail.com>
2013-08-15 15:33:51 -0700

    made basic_auth optional

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,8 +1,10 @@
 class EmailsController < ApplicationController
   skip_before_filter :verify_authenticity_token
-  http_basic_authenticate_with name:     Secret.basic_auth_name || ENV["BASIC_AUTH_NAME"], 
-                               password: Secret.basic_auth_pass || ENV["BASIC_AUTH_PASS"],
-                               only: :authorized_show
+  if basic_auth_exists?
+    http_basic_authenticate_with name:     Secret.basic_auth_name || ENV["BASIC_AUTH_NAME"], 
+                                 password: Secret.basic_auth_pass || ENV["BASIC_AUTH_PASS"],
+                                 only: :authorized_show
+  end
 
   def history
     email = Email.find(params[:id])
@@ -29,5 +31,11 @@ class EmailsController < ApplicationController
   def new
     @email = Email.new
   end
+
+  protected
+
+    def basic_auth_exists?
+      if (Secret.basic_auth_name && Secret.basic_auth_pass) || (ENV["BASIC_AUTH_NAME"] && ENV["BASIC_AUTH_PASS"])
+    end
 
 end
